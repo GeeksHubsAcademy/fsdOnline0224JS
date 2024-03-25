25a95f65c1c305c58245a84113825010464101c4
Dave86dev <davidochando1986@gmail.com>
2024-03-25 20:29:51 +0100

    primeros pasos JS

---
+++ js/main.js
@@ -0,0 +1,139 @@
+
+// console.log("Hola, soy console log.");
+
+// console.log("Hola, soy otro console log.");
+
+// let numero = 55;
+// const numero2 = 100;
+
+// numero = 44;
+
+// numero = 22;
+
+// numero = 53;
+
+// console.log(numero)
+// console.log(numero2)
+
+////////////////////////////////////////
+//JAVASCRIPT 25/03/2024/////////////////
+////////////////////////////////////////
+
+//1DECLARACION DE VARIABLES Y OPERACIONES BÁSICAS
+
+// let numero1 = 544;
+
+// let numero2 = 455;
+
+// console.log(numero1 + numero2);
+
+// let resultado = numero1 + numero2;
+
+// resultado = 100;
+
+// console.log(resultado)
+
+//2OPERACIONES BÁSICAS ARITMÉTICAS
+
+// let numero1 = 100;
+// let numero2 = 500;
+
+// let resultado = numero1 - numero2
+// console.log("El resultado de la resta es.... ", resultado)
+
+// resultado = numero1 + numero2
+// console.log("El resultado de la suma es....", resultado)
+
+// resultado = numero1 * numero2 
+// console.log("El resultado de la multiplicacion es....", resultado)
+
+// resultado = numero1 / numero2
+// console.log("El resultado de la division es.....", resultado)
+
+// //MÓDULO
+
+// resultado = 17 % 2;
+// console.log("El resultadod del módulo es.....", resultado)
+
+// //Cerciórate de si un número es par o impar
+
+// if(resultado !== 0){
+
+//     console.log("No se trata de un número par.")
+
+
+// }
+
+//ESTRUCTURA DE CONTROL DE PROGRAMACIÓN CONDICIONAL "IF"
+
+// let nombre = "Pepe"
+
+// if(nombre === "Daniel"){
+
+//     console.log("Hola Daniel")
+
+// }else{
+
+//     console.log("Tu no eres Daniel")
+// }
+
+// let numero1 = 10;
+
+// let numero2 = 35;
+
+// let resultado = numero1 + numero2;
+
+// if(resultado > 80){
+//     console.log("Si, resultado es mayor que 20")
+// }
+
+//OPERACIONES ARITMETICAS DE INCREMENTO Y DECREMENTO
+
+// let numero = 5;
+
+// numero++;
+// console.log(numero)
+
+// numero--;
+// console.log(numero)
+
+
+// let numero = prompt("Dime el número que quieras");
+
+// if(numero > 10){
+//     console.log("El número es mayor a 10.")
+// }
+
+
+//NEGACION UNARIA Y UNARIO POSITIVO
+
+// let numero = 5;
+
+// let numero2 = -2;
+
+// console.log(-numero2);
+
+//OPERADORES LÓGICOS
+
+//&& ... and...(si)
+
+let numero = 10;
+
+if((numero > 2) && (numero < 20)){
+    console.log("Hola, habremos entendido esto??")
+}
+
+// || .... or...(o)
+
+let numero2 = 20;
+
+if((numero2 > 1000) || (numero2 < 21)){
+    console.log("Hola, esto también????")
+}
+
+let variable3 = false;
+
+if(!variable3){
+    console.log("esto saldría???")
+}
+
